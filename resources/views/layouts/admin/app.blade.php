9c013b8b8ae2976c7468cadce3f2b3179577d5a0
Aditya Wanwade <Adiwanwade@gmail.com>
2023-12-11 13:12:13 +0530

    Added categories feature

--- resources/views/layouts/admin/app.blade.php
+++ resources/views/layouts/admin/app.blade.php
@@ -54,10 +54,19 @@
                         >All Users</a
                     >
                     <a
-                    
+
                     href="{{ route('admin-users-create') }}"
                     class="block px-4 py-2 hover:bg-indigo-800 rounded-md"
-                        >Create User</a
+                        >Create User</a>
+                        <a
+                        href="{{ route('admin-categories') }}"
+                        class="block px-4 py-2 hover:bg-indigo-800 rounded-md"
+                        >All Categories</a
+                    >
+                    <a
+                        href="{{ route('admin-categories-create') }}"
+                        class="block px-4 py-2 hover:bg-indigo-800 rounded-md"
+                        >Create Category</a
                     >
                     <a
                         href="#"
