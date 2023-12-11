a1f93ff71049bcc076ec4164eef7856a73a8f5ee
Aditya Wanwade <Adiwanwade@gmail.com>
2023-12-11 09:10:41 +0530

    Added user editing feature

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@ use App\Http\Controllers\AdminController;
 use App\Http\Controllers\AdminUsersController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,33 +13,35 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
 Route::get('/', function () {
     return view('welcome');
 });
-
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-Route::get('/admin', [AdminController::class, 'index'])->name('admin');
-
+require __DIR__.'/auth.php';
+Route::get('/admin', [
+    AdminController::class, 'index'
+])->middleware('auth')->name('admin');
 Route::get('/admin/users', [
     AdminUsersController::class, 'index'
 ])->middleware('auth')->name('admin-users');
 Route::get('/admin/users/create', [
     AdminUsersController::class, 'create'
 ])->middleware('auth')->name('admin-users-create');
-
-
 Route::post('/admin/users/create', [
     AdminUsersController::class, 'store'
 ])->middleware('auth')->name('admin-users-create');
 
+Route::get('/admin/users/{id}/edit', [
+    AdminUsersController::class, 'edit'
+])->middleware('auth')->name('admin-users-edit');
 
-require __DIR__.'/auth.php';
+Route::patch('/admin/users/{id}/edit', [
+    AdminUsersController::class, 'update'
+])->middleware('auth')->name('admin-users-edit');
